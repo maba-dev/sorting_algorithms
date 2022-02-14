4c24b80e72577b1082937d1a146d5c27df7b09f2
maba-dev <cherif.ba@hotmail.fr>
2022-02-14 20:21:12 +0100

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,46 @@
 #include "sort.h"
 /**
- * @brief 
- * 
+ * swap - The swap function
+ * @array: The array to swap
+ * @idx: The idx to array
+ * Return: None
  */
 
 void swap(int *array, int idx)
 {
-    int tmp;
+	int tmp;
 
-    tmp = array[idx];
-    array[idx] = array[idx + 1];
-    array[idx + 1] = tmp;
+	tmp = array[idx];
+	array[idx] = array[idx + 1];
+	array[idx + 1] = tmp;
 }
+
+/**
+ * bubble_sort - a function that sorts an array of integers in ascending order
+ * @array: The array to swap
+ * @size: The size to array
+ * Return: None
+ */
 
 void bubble_sort(int *array, size_t size)
 {
-    int index;
-    size_t i ;
-    size_t j;
+	int index;
+	size_t i;
+	size_t j;
 
 
-    for (i = 0; i < size; i++)
-    {
-        j = 0;
-        for (index = 0; j < size; index++)
-        {
-            if (array[index] > array[index + 1] && j + 1 < size)
-            {
-                /**printf("\navant swap\n");
-                print_array(array, size);**/
-                swap(array, index);
-                
-                print_array(array, size);
-            }
-            j++;
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		j = 0;
+		for (index = 0; j < size; index++)
+		{
+			if (array[index] > array[index + 1] && j + 1 < size)
+			{
+				swap(array, index);
+				print_array(array, size);
+			}
+			j++;
+		}
+	}
 
 }
