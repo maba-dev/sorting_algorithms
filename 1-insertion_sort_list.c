0509fc331d58c43fdaac53eec050363216da74f3
maba-dev <cherif.ba@hotmail.fr>
2022-02-15 11:21:40 +0100

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,49 @@
 #include "sort.h"
 
 /**
- * @brief 
+ *
+ * 
  * 
  */
-void swap(listint_t *actual_nodes, listint_t *prev)
+
+void swap(listint_t **list, listint_t *actual_nodes, listint_t *prev)
 {
-    listint_t *tmp;
-    tmp = actual_nodes;
-    actual_nodes = prev;
-    prev = tmp;
+	if (prev->prev)
+		prev->prev->next = actual_nodes;
+	else
 
+		*list = actual_nodes;
+	if (actual_nodes->next)
+		actual_nodes->next->prev = prev;
+	prev->next = actual_nodes->next;
+	actual_nodes->next = prev;
+	actual_nodes->prev = prev->prev;
+	prev->prev = actual_nodes;
 }
-
+/**
+ *insertion_sort_list  - a function that sorts a doubly linked list of integers in ascending order
+ * @array: The array to swap
+ * @size: The size to array
+ * Return: None
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *actual_nodes;
-    listint_t *head = *list;
+	listint_t *actual_nodes;
+	listint_t *head = *list;
 
-    head->prev = NULL;
-    actual_nodes = head->next;
-    actual_nodes->prev = head;
-    while (actual_nodes!= NULL)
-    {
-        while(actual_nodes->prev && (actual_nodes->n < actual_nodes->prev->n))
-        {
-            swap(actual_nodes, actual_nodes->prev);
-            print_list(*list);
-        }
+	head->prev = NULL;
+	actual_nodes = head->next;
+	actual_nodes->prev = head;
+	while (actual_nodes!= NULL)
+	{
+		while(actual_nodes->prev && (actual_nodes->n < actual_nodes->prev->n))
+		{
+			swap(list, actual_nodes, actual_nodes->prev);
+			print_list(*list);
+		}
 
-        actual_nodes = actual_nodes->next;
-    }
+		actual_nodes = actual_nodes->next;
+	}
 
 }
